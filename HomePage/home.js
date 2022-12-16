7d528622dc5e79efc4dee0d92fe3f889122ef23e
baltao1 <akashchauhan8744@gmail.com>
2022-12-16 23:02:41 +0530

    day-3

---
+++ HomePage/home.js
@@ -0,0 +1,11 @@
+var fs = new Splide( '#first-slider', {
+    type : 'loop',
+    arrows : true,
+    autoplay : true,
+} ).mount();
+
+var ss = new Splide( '#second-slider', {
+    type : 'loop',
+    arrows : true,
+    autoplay : true,
+}).mount();
